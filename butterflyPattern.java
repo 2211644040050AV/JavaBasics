51e941d650a4f9ccf8a7fe8fe446e6f57d025388
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-09-10 20:10:47 +0530

    ButterFly Pattern Program

---
+++ butterflyPattern.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+// Butterfly Pattern 
+public class butterflyPattern {
+    public static void butterFly(int n) {
+        // 1st half
+        for (int i = 1; i <= n; i++) {
+            // stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // stars - 2 * (n - i)
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 2ns half
+        for (int i = n; i >= 1; i--) {
+            // stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // stars - 2 * (n - i)
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer number : ");
+        int n = sc.nextInt();
+        butterFly(n);
+        sc.close();
+    }
+}
