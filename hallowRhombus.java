ec568aaa9ecb1c433f33c091174ef1e3dca2155e
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-09-12 18:36:45 +0530

    Hallow Rhombus

---
+++ hallowRhombus.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+// Hallow Rhombus
+
+public class hallowRhombus {
+    public static void hallow_Rhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // hallow rectangle
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer number : ");
+        int n = sc.nextInt();
+        hallow_Rhombus(n);
+        sc.close();
+    }
+}
