232aa4971d6261b343dd9c7c47224dafa321440e
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-09-26 17:42:42 +0530

    Kadanes Algoruthm

--- maxSubArray.java
+++ maxSubArray.java
@@ -24,34 +24,50 @@ public class maxSubArray {
     // }
 
     // max sub array prefix sum
-    public static void Print_SubArr(int Arr[]) {
+    // public static void Print_SubArr(int Arr[]) {
+    // int currSum = 0;
+    // int maxSum = Integer.MIN_VALUE;
+    // int prefix[] = new int[Arr.length];
+    // prefix[0] = Arr[0];
+
+    // // calculate prefix array
+    // for (int i = 1; i < Arr.length; i++) {
+    // prefix[i] = prefix[i - 1] + Arr[i];
+    // }
+
+    // for (int i = 0; i < Arr.length; i++) {
+    // int start = i;
+    // for (int j = i; j < Arr.length; j++) {
+    // int end = j;
+    // currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+
+    // if (maxSum < currSum) {
+    // maxSum = currSum;
+    // }
+    // }
+    // }
+    // System.out.println("Max Sum = " + maxSum);
+    // }
+
+    // Kadanes Algorithm
+    public static void kandanes(int Arr[]) {
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
-        int prefix[] = new int[Arr.length];
-        prefix[0] = Arr[0];
-
-        // calculate prefix array
-        for (int i = 1; i < Arr.length; i++) {
-            prefix[i] = prefix[i - 1] + Arr[i];
-        }
-
         for (int i = 0; i < Arr.length; i++) {
-            int start = i;
-            for (int j = i; j < Arr.length; j++) {
-                int end = j;
-                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
-
-                if (maxSum < currSum) {
-                    maxSum = currSum;
-                }
+            currSum = currSum + Arr[i];
+            if (currSum < 0) {
+                currSum = 0;
             }
+            maxSum = Math.max(currSum, maxSum);
         }
-        System.out.println("Max Sum = " + maxSum);
+        System.out.println("Your max subarry sum is : " + maxSum);
     }
 
     public static void main(String[] args) {
-        int Arr[] = { 2, 4, 6, 8, 10 };
-        Print_SubArr(Arr);
+        // int Arr[] = { 2, 4, 6, 8, 10 };
+        // Print_SubArr(Arr);
+        int Arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        kandanes(Arr);
     }
 
 }
