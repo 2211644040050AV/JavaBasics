fbdfa3f5bf334fb7db6102bae38aa8aae0ddf37c
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-10-03 18:14:20 +0530

    write a boolean search function in matrices program

--- Matrices.java
+++ Matrices.java
@@ -2,6 +2,19 @@ import java.util.*;
 
 public class Matrices {
 
+    public static boolean search(int matrix[][], int n, int m, int key) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.print("Key found at index [" + i + "] [" + j + "]");
+                    return true;
+                }
+            }
+        }
+        System.out.println("Key not found!");
+        return false;
+    }
+
     public static void main(String[] args) {
         int[][] matrix = new int[3][3];
         int n = matrix.length, m = matrix[0].length;
@@ -22,6 +35,8 @@ public class Matrices {
             }
             System.out.println();
         }
+
+        search(matrix, n, m, 5);
 
         sc.close();
     }
