a6065c9981641b85ba23337eceeeead3210b9b3f
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-10-08 06:07:17 +0530

    String is palindrome or not

---
+++ PalindromeString.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static boolean isPalindrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            int n = str.length();
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                // not a palindrome
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any string: ");
+        String str = sc.nextLine();
+
+        // Call the method and print the result
+        if (isPalindrome(str)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
