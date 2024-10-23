60c84a47755723506a91ec1f964bb82c34b13909
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-10-23 22:04:22 +0530

    Clear Range of Bits

--- ClearLastIthBit.java
+++ ClearLastIthBit.java
@@ -4,7 +4,16 @@ public class ClearLastIthBit {
         return n & bitMask;
     }
 
+    // Clear Range of Bits
+    public static int ClearRangeOfBits(int n, int i, int j) {
+        int a = ((~0) << (j + 1));
+        int b = (1 << i) - 1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
+
     public static void main(String[] args) {
-        System.out.println(clear_Last_Ith_Bit(15, 2));
+        // System.out.println(clear_Last_Ith_Bit(15, 2));
+        System.out.println(ClearRangeOfBits(10, 2, 4));
     }
 }
