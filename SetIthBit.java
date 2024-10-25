b2393781019a67ae2cfb2737a2b69d6ff00aa0db
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-10-25 17:23:05 +0530

    Count Set Bits in a nimber.

--- SetIthBit.java
+++ SetIthBit.java
@@ -4,7 +4,20 @@ public class SetIthBit {
         return n | bitMask;
     }
 
+    // Count Set Bits in a nimber.
+    public static int countSetBit(int n) {
+        int count = 0;
+        while (n > 0) { // Check oue LSB
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
-        System.out.println(set_Ith_Bit(10, 2));
+        // System.out.println(set_Ith_Bit(10, 2));
+        System.out.println(countSetBit(10));
     }
 }
