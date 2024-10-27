0f4b04c8782ffbd11294b0341a3eb5bbacfd3cda
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-10-27 23:36:03 +0530

    Fast Exponential

--- SetIthBit.java
+++ SetIthBit.java
@@ -16,8 +16,22 @@ public class SetIthBit {
         return count;
     }
 
+    // Fast Exponential
+    public static int fastExpo(int a, int n) {
+        int ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) { // check LSG
+                ans = ans * a;
+            }
+            a = a * a;
+            n = n >> 1;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         // System.out.println(set_Ith_Bit(10, 2));
-        System.out.println(countSetBit(10));
+        // System.out.println(countSetBit(10));
+        System.out.println(fastExpo(3, 5));
     }
 }
