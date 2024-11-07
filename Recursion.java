8484a2faccc716ea59e4b9d0e71fa0a7d7106222
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-11-07 20:19:53 +0530

    Factorial Program

---
+++ Recursion.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Recursion {
+
+    public static int factorial(int n) {
+
+        if (n <= 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int result = factorial(number);
+
+        System.out.println("Factorial of " + number + " is: " + result);
+
+        scanner.close();
+    }
+}
