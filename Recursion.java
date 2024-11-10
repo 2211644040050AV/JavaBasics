fce632a6a7cc84cc7df340b36af0c918dd709eab
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-11-10 22:14:54 +0530

    sum of first n natural numbers

--- Recursion.java
+++ Recursion.java
@@ -30,9 +30,18 @@ public class Recursion {
         return n * factorial(n - 1);
     }
 
+    // sum of first n natural numbers
+    public static int sumOfNaturalNumbers(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n + sumOfNaturalNumbers(n - 1);
+    }
+
     public static void main(String[] args) {
-        int n = 10;
-        printInc(n);
+        int n = 5;
+        // printInc(n);
+        System.out.println(sumOfNaturalNumbers(n));
         Scanner scanner = new Scanner(System.in);
 
         // System.out.print("Enter a number: ");
