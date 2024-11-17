369118ad6b428d874a3045bfab3aed35fb5f4eeb
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-11-17 20:20:37 +0530

    Tiling Problem

--- Recursion.java
+++ Recursion.java
@@ -109,6 +109,28 @@ public class Recursion {
         return halfPowerSqr;
     }
 
+    // Tiling Problem -
+    /*
+     * Given a "2 * n" board and tiles of size "2 * 1", count the number of ways to
+     * tiles the given board using the 2 * 1 tiles. (A tile can either be placed
+     * horizontally or vertically.)
+     */
+    public static int tilingProblem(int n) { // 1 * n (floor size)
+        // base case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        // vertical choice
+        int fnm1 = tilingProblem(n - 1);
+
+        // horizontal choice
+        int fnm2 = tilingProblem(n - 2);
+
+        int totalWays = fnm1 + fnm2;
+        return totalWays;
+
+    }
+
     public static void main(String[] args) {
         // int n = 25;
         // printInc(n);
@@ -118,9 +140,11 @@ public class Recursion {
         // int[] arr = { 5, 5, 5, 5 };
         // System.out.println(lastOccurance(arr, 5, 0));
 
-        int a = 2;
-        int n = 10;
-        System.out.println(optimizedPower(a, n));
+        // int a = 2;
+        // int n = 10;
+        // System.out.println(optimizedPower(a, n));
+
+        System.out.println(tilingProblem(4));
 
         Scanner scanner = new Scanner(System.in);
 
