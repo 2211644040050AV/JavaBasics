d9633be40472fbcdc55eb0462b6ab03ddb9488f0
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-12-03 07:13:23 +0530

    remove last:

--- LinkedList.java
+++ LinkedList.java
@@ -99,6 +99,28 @@ public class LinkedList {
         return val;
     }
 
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("Linked list is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        // previous : i = size - 2;
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data; // tail.data
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.addFirst(2);
@@ -107,7 +129,7 @@ public class LinkedList {
         ll.addLast(5);
         ll.addMiddle(2, 3);
         ll.printList();
-        ll.removeFirst();
+        ll.removeLast();
         ll.printList();
 
         System.out.println(ll.size); // prins the size of the linked list
