7ec4f9ef85489b072a48b8ec54a6d79f942371ac
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-12-12 10:26:37 +0530

    use java collection Framework for creating a linked list

---
+++ CollectionFramework.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class CollectionFramework {
+    public static void main(String[] args) {
+        // create - objects int, float, boolean is not used -> Integer, Float, Character
+        // is used,
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+
+        System.out.println(ll);
+
+        // add
+
+        // remove
+    }
+}
