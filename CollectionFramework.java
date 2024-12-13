6f46ff6720a727bbaf0b98c98a8d60c143543155
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-12-13 23:06:07 +0530

    remove in linked list

--- CollectionFramework.java
+++ CollectionFramework.java
@@ -4,16 +4,20 @@ public class CollectionFramework {
     public static void main(String[] args) {
         // create - objects int, float, boolean is not used -> Integer, Float, Character
         // is used,
+
+        // create
         LinkedList<Integer> ll = new LinkedList<>();
 
+        // add
         ll.addLast(1);
         ll.addLast(2);
         ll.addFirst(0);
-
         System.out.println(ll);
 
-        // add
-
         // remove
+        ll.removeLast();
+        ll.removeFirst();
+        System.out.println(ll);
+
     }
 }
