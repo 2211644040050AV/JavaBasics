622110f1cfe2cfdc9ffd84deee3b587fa627bb15
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2024-12-16 14:38:05 +0530

    perform addFirst in a doubly linked lis

---
+++ DoubleLL.java
@@ -0,0 +1,56 @@
+public class DoubleLL {
+    // Doubly Linked List
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    // Add a node at the beginning
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    // Print the list
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DoubleLL dll = new DoubleLL();
+
+        // Add elements to the doubly linked list
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+
+        // Print the list
+        dll.print();
+
+        // Print the size of the list
+        System.out.println("Size of the list: " + dll.size);
+    }
+}
